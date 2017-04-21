50af1b313cbda679edb2c3f35de656ccea662b36
Rover656 <reece-mackie@hotmail.com>
2017-04-21 13:35:37 +0100

    Begin Session Class (Hopefully it works)

--- core/classes/Session.php
+++ core/classes/Session.php
@@ -1,5 +1,22 @@
 <?php
 class Session {
-   //THIS WILL HANDLE ALL SESSION DATA FOR US
+   public function getUsername() {
+      session_start();
+      $val = $_SESSION["Username"];
+      session_destroy();
+      return $val;
+   }
+   public function getToken() {
+      session_start();
+      $val = $_SESSION["Token"];
+      session_destroy();
+      return $val;
+   }
+   public function getBasket() {
+      session_start();
+      $val =  $_SESSION["Basket"];
+      session_destroy();
+      return $val;
+   }
 }
 ?>
